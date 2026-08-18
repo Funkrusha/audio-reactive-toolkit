55ff8e2304617b774e6536198b1ffe3666539e4d
Funkrusha <mail@funkrusha.net>
2026-08-18 22:35:40 +0200

    Unify SPDX-* comments

--- src/plugin-main.cpp
+++ src/plugin-main.cpp
@@ -1,13 +1,5 @@
-/*
-Audio Reactive Toolkit
-Copyright (C) 2026 Funkrusha
-SPDX-License-Identifier: GPL-3.0-or-later
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 3 of the License, or
-(at your option) any later version.
-*/
+// SPDX-FileCopyrightText: 2026 Funkrusha
+// SPDX-License-Identifier: GPL-3.0-or-later
 
 #include <obs-frontend-api.h>
 #include <obs-module.h>
